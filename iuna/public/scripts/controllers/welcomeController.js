769d2e4741ae51d572d9b1a9a9fd0350c65c2bdf
Lucas <lpiscello@gmail.com>
2015-06-02 23:32:45 -0300

    Iuna's station map ftw :)

--- iuna/public/scripts/controllers/welcomeController.js
+++ iuna/public/scripts/controllers/welcomeController.js
@@ -4,7 +4,15 @@
 
     function welcomeCtrl($scope) {
 
-        $scope.helloMessage = 'What is up! My name is Iuna and I\'m fucking alive!';
+        function createMap() {
+
+            cartodb.createVis('map', 'https://lpiscello.cartodb.com/api/v2/viz/440cf4e8-0982-11e5-9041-0e0c41326911/viz.json', {
+                search: false,
+                shareable: false
+            });
+        }
+
+        createMap();
     }
 
     angular.module('iunaApp.controllers').controller('WelcomeController', welcomeCtrl);
