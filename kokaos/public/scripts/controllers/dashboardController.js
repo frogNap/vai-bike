2fbbd01e766ae5221c58b0a01c5dff0757fe4d6d
Lucas <lpiscello@gmail.com>
2015-06-06 17:09:08 -0300

    Map on real time -- i hope anyways

--- kokaos/public/scripts/controllers/dashboardController.js
+++ kokaos/public/scripts/controllers/dashboardController.js
@@ -2,14 +2,22 @@
 
     'use strict';
 
-    function dashboardCtrl() {
+    function dashboardCtrl($interval) {
 
         function createMap() {
 
-            cartodb.createVis('map', 'https://lpiscello.cartodb.com/api/v2_1/viz/8cdfae08-0c12-11e5-b5a2-0e853d047bba/viz.json', {
-                search: false,
-                shareable: false
-            });
+            cartodb.createVis('map', 'https://lpiscello.cartodb.com/api/v2_1/viz/8cdfae08-0c12-11e5-b5a2-0e853d047bba/viz.json',
+                {
+                    search: false,
+                    shareable: false
+                })
+                .done(function(vis, layers) {
+
+                    $interval(function() {
+
+                        layers[1].invalidate();
+                    }, 10000);
+                });
         }
 
         createMap();
